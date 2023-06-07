e9a6991385517c0cc06e76eb242c7b1bf522b454
EtoPashka <48890848+EtoPashka@users.noreply.github.com>
2023-06-07 19:48:22 +0300

    fixes

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require('discord.js');
 require('dotenv/config');
-const fs = require('node:fs');
+const fs = require('fs');
 const path = require('node:path');
 
 const commands = [];
